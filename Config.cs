de762e368060caec1c251a66f56f7267c4c5ee4c
Toren Darby <darby@science.ubc.ca>
2018-12-22 16:39:15 -0800

    1.0.0 Commit

---
+++ Config.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Deltin.CustomGameAutomation;
+using YamlDotNet.RepresentationModel;
+
+public class Config
+{
+    public readonly string BattlenetExecutableFilePath;
+    public readonly string BlueName;
+
+    public readonly List<Tuple<AIHero, Difficulty>> Bots;
+
+    public readonly string EndMessage1st;
+    public readonly string EndMessage2nd;
+
+    public readonly int NumberPlayersWhenBotsAreRemoved;
+    public readonly string OverwatchSettingsFilePath;
+
+    public readonly int PresetLocation;
+    public readonly string RedName;
+    public readonly string ServerName;
+
+    private YamlMappingNode _mapping;
+
+    public Config()
+    {
+        InitMapping();
+        BattlenetExecutableFilePath = InitField("BattlenetExecutableFilePath");
+        OverwatchSettingsFilePath = InitFieldOrFallback("OverwatchSettingsFilePath", @"C:\Users\" + Environment.UserName + @"\Documents\Overwatch\Settings\Settings_v0.ini");
+        ServerName = InitField("ServerName");
+        BlueName = InitField("BlueName");
+        RedName = InitField("RedName");
+        EndMessage1st = InitField("EndMessage1st");
+        EndMessage2nd = InitField("EndMessage2nd");
+
+        NumberPlayersWhenBotsAreRemoved = int.Parse(InitField("NumberPlayersWhenBotsAreRemoved"));
+
+        PresetLocation = int.Parse(InitField("PresetLocation"));
+        Bots = InitBots();
+        if (GetNumTotalBots() + NumberPlayersWhenBotsAreRemoved > 12)
+        {
+            throw new BotsMayNotAllFitException(GetNumTotalBots(), NumberPlayersWhenBotsAreRemoved);
+        }
+    }
+
+    public int GetNumTotalBots()
+    {
+        return Bots.Count * 2;
+    }
+
+    private void InitMapping()
+    {
+        string cfgString = File.ReadAllText("cfg.yaml");
+        StringReader cfgStreamReader = new StringReader(cfgString);
+
+        YamlStream yaml = new YamlStream();
+        yaml.Load(cfgStreamReader);
+
+        // Examine the stream
+        _mapping = (YamlMappingNode) yaml.Documents[0].RootNode;
+    }
+
+    private string InitFieldOrFallback(string fieldName, string fallback)
+    {
+        try
+        {
+            return InitField(fieldName);
+        }
+        catch (KeyNotFoundException)
+        {
+            return fallback;
+        }
+    }
+
+    private string InitField(string fieldName)
+    {
+        return (string) _mapping.Children[new YamlScalarNode(fieldName)];
+    }
+
+    private List<Tuple<AIHero, Difficulty>> InitBots()
+    {
+        List<Tuple<AIHero, Difficulty>> bots = new List<Tuple<AIHero, Difficulty>>();
+
+        var botsNode = (YamlSequenceNode) _mapping.Children[new YamlScalarNode("Bots")];
+        foreach (YamlMappingNode bot in botsNode)
+        {
+            string heroString = (string) bot.Children[new YamlScalarNode("Hero")];
+            AIHero hero = StringToHero(heroString);
+
+            string difficultyString = (string) bot.Children[new YamlScalarNode("Difficulty")];
+            Difficulty difficulty = StringToDifficulty(difficultyString);
+
+            Tuple<AIHero, Difficulty> botTuple = new Tuple<AIHero, Difficulty>(hero, difficulty);
+            bots.Add(botTuple);
+        }
+
+        return bots;
+    }
+
+    private static AIHero StringToHero(string heroString)
+    {
+        switch (heroString)
+        {
+            case "Ana":
+                return AIHero.Ana;
+            case "Bastion":
+                return AIHero.Bastion;
+            case "Lucio":
+                return AIHero.Lucio;
+            case "McCree":
+                return AIHero.McCree;
+            case "Mei":
+                return AIHero.Mei;
+            case "Reaper":
+                return AIHero.Reaper;
+            case "Recommended":
+                return AIHero.Recommended;
+            case "Roadhog":
+                return AIHero.Roadhog;
+            case "Soldier76":
+                return AIHero.Soldier76;
+            case "Sombra":
+                return AIHero.Sombra;
+            case "Torbjorn":
+                return AIHero.Torbjorn;
+            case "Zarya":
+                return AIHero.Zarya;
+            case "Zenyatta":
+                return AIHero.Zenyatta;
+            default:
+                throw new BadHeroNameException(heroString);
+        }
+    }
+
+    private static Difficulty StringToDifficulty(string difficultyString)
+    {
+        switch (difficultyString)
+        {
+            case "Easy":
+                return Difficulty.Easy;
+            case "Medium":
+                return Difficulty.Medium;
+            case "Hard":
+                return Difficulty.Hard;
+            default:
+                throw new BadDifficultyNameException(difficultyString);
+        }
+    }
+}
+
+public class BadHeroNameException : Exception
+{
+    public BadHeroNameException(string name) : base(
+        $"{name} is not a valid bot hero name. See the config file for a list of valid bot hero names.")
+    {
+    }
+}
+
+public class BadDifficultyNameException : Exception
+{
+    public BadDifficultyNameException(string name) : base(
+        $"{name} is not a valid bot difficulty name. See the config file for a list of valid bot difficulty names.")
+    {
+    }
+}
+
+public class BotsMayNotAllFitException : Exception
+{
+    public BotsMayNotAllFitException(int numBots, int numPlayersToRemoveBots) : base(
+        $"{numBots} bots are requested. Bots are removed when there are {numPlayersToRemoveBots} players. " +
+        $"{numBots} + {numPlayersToRemoveBots} is greater than the total number of slots (12)." +
+        $"Note that bots are added to both teams."
+    ){}
+}
---
+++ GameLoop.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Deltin.CustomGameAutomation;
+
+internal class GameLoop
+{
+    private static int _serverTime = 0;
+    public readonly Config Cfg;
+    private readonly bool _performFirstTimeSetup;
+    public CustomGame Cg;
+
+    public GameLoop(Config cfg)
+    {
+        Cfg = cfg;
+        Process process;
+        if (OverwatchIsOpen())
+        {
+            _performFirstTimeSetup = false;
+            process = CustomGame.GetOverwatchProcess();
+        }
+        else
+        {
+            _performFirstTimeSetup = true;
+            process = CreateNewOverwatchProcess();
+        }
+
+        Cg = CreateCustomGame(process);
+    }
+
+    private Process CreateNewOverwatchProcess()
+    {
+        var info = new OverwatchProcessInfoAuto
+        {
+            BattlenetExecutableFilePath = Cfg.BattlenetExecutableFilePath,
+            OverwatchSettingsFilePath = Cfg.OverwatchSettingsFilePath,
+        };
+        Process process = CreateNewOverwatchProcessFromInfo(info);
+        return process;
+    }
+
+    private Process CreateNewOverwatchProcessFromInfo(OverwatchProcessInfoAuto info)
+    {
+        Process process = null;
+        while (process == null)
+        {
+            try
+            {
+                process = CustomGame.CreateOverwatchProcessAutomatically(info);
+            }
+            catch (OverwatchStartFailedException){}
+        }
+
+        return process;
+
+    }
+
+    private CustomGame CreateCustomGame(Process owProcess)
+    {
+        CustomGameBuilder builder = new CustomGameBuilder
+        {
+            OpenChatIsDefault = false,
+            OverwatchProcess = owProcess,
+        };
+
+        return new CustomGame(builder);
+    }
+
+    private bool OverwatchIsOpen()
+    {
+        bool overwatchIsOpen = CustomGame.GetOverwatchProcess() != null;
+        return overwatchIsOpen;
+    }
+
+    private void FirstTimeSetup()
+    {
+        Cg.Settings.SetJoinSetting(Join.Everyone);
+
+        Cg.Settings.SetGameName(Cfg.ServerName);
+        Cg.Settings.SetTeamName(Team.Blue, $@"\ {Cfg.BlueName}");
+        Cg.Settings.SetTeamName(Team.Red, $"* {Cfg.RedName}");
+
+        SwapHostToSpectate();
+
+        Cg.Settings.LoadPreset(Cfg.PresetLocation);
+        Cg.Chat.SwapChannel(Channel.Match);
+
+        Cg.StartGame();
+
+        Program.EnterPhase(typeof(First30SecondsPhase));
+    }
+
+    private void ExistingGameSetup()
+    {
+        Cg.AI.RemoveAllBotsAuto();
+        Program.EnterPhase(typeof(GamePhase));
+    }
+
+
+    public void Start()
+    {
+        Setup();
+        Loop();
+    }
+
+    private void Setup()
+    {
+        if (_performFirstTimeSetup)
+        {
+            FirstTimeSetup();
+        }
+        else
+        {
+            ExistingGameSetup();
+        }
+    }
+
+    private void SwapHostToSpectate()
+    {
+        Cg.Interact.Move(0, 12);
+    }
+
+    private static void Loop()
+    {
+        Debug.WriteLine("Starting loop");
+        while (true)
+        {
+            foreach (int delay in Program.Phase.LoopFuncs.Keys)
+            {
+                if (_serverTime % delay == 0)
+                {
+                    try
+                    {
+                        foreach (Action func in Program.Phase.LoopFuncs[delay])
+                        {
+                            func();
+                        }
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                    }
+                }
+            }
+
+            _serverTime += 1;
+            Program.TimeAtCurrentMap += 1;
+            Thread.Sleep(500);
+        }
+    }
+}
